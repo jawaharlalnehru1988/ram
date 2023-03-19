a09f3a578210725a2d3b5c773ddfcfccddabba49
Jawaharlal_nehru123 <jawaharlal@wolkensoftware.com>
2023-03-19 07:38:01 +0530

    some bugs fixed in the japa

--- src/app/japa/japa.component.ts
+++ src/app/japa/japa.component.ts
@@ -1,5 +1,5 @@
 import { Component,  OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { KirtanDialogComponent } from '../kirtan-lesson/kirtan-dialog/kirtan-dialog.component';
 
 
@@ -14,7 +14,7 @@ export class JapaComponent implements OnInit {
   malaCounting = 0;
   beatCounting = 0;
   sixteenMalaCount = 0;
-  isAudioPassed = false;
+  isAudioPaused = false;
   count: number=0;
   comparisonCount: any;
   timeElements: any;
@@ -26,6 +26,7 @@ export class JapaComponent implements OnInit {
   isPlayingsri: boolean = false;
   fullMala = 108;
   loopAudio:any;
+  dialogRef!: MatDialogRef<KirtanDialogComponent>;
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
@@ -71,8 +72,12 @@ playTickSound(){
     jaya.src = '../assets/mp3/jayaSri.mp3';
     jaya.load();
     jaya.play();
+    let chaitanyaManthra = 'ஸ்ரீ கிருஷ்ண சைதன்ய ப்ரபு நித்யானந்த ஸ்ரீ அத்வைத கதாதர ஸ்ரீ வாஸாதி கௌர பக்த விருந்த '
+    this.openDialog(chaitanyaManthra)
     jaya.addEventListener('ended', () =>{
       this.isPlayingsri = false;
+      this.dialogRef.close();
+      this.playAudio(0);
     })
   }
   reset16Count(){
@@ -93,29 +98,35 @@ if (isChecked === true) {
 }
   }
   loop108Count(event:any){
-    this.isAudioPassed = !this.isAudioPassed;
+    this.isAudioPaused = !this.isAudioPaused;
     this.prabhuVoiceEnabled = false;
+console.log("event", event);
 
-      this.loopAudio = new Audio();
-    this.loopAudio.src = '../assets/mp3/prabhupadaChant.mp3';
-    this.loopAudio.load();
-    if (this.isAudioPassed) {
-    this.loopAudio.play();
-    } else {
-      this.openDialog();
-      this.loopAudio.pause();
-    }
-    this.loopAudio.addEventListener('ended', ()=>{
-      this.playAudio(1);
-    })
-    
-    
+if(this.beatCounting === 0){
+  this.jayaSriKrishna();
+} else {
+  this.loopAudio = new Audio();
+  this.loopAudio.src = '../assets/mp3/prabhupadaChant.mp3';
+  this.loopAudio.load();
+  if (this.isAudioPaused) {
+  this.loopAudio.play();
+  this.isPlaying = true;
+  this.isPlayingsri = true;
+} else {
+  let message = "இந்த மந்திரம் முடிந்த பிறகு  நிறுத்தப்படும். ஏனென்றால் மந்திரத்தை இடையில் நிறுத்தக்கூடாது"
+  this.openDialog(message);
+  this.loopAudio.pause();
   }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(KirtanDialogComponent, {
+  this.loopAudio.addEventListener('ended', ()=>{
+    this.playAudio(1);
+  })
+}
+  }
+  openDialog(optional:any): void {
+    this.dialogRef = this.dialog.open(KirtanDialogComponent, {
       width: '250px',
       data: {
-        message: "இந்த மந்திரம் முடிந்த பிறகு  நிறுத்தப்படும். ஏனென்றால் மந்திரத்தை இடையில் நிறுத்தக்கூடாது",
+        message: optional,
         id: "japa"
       }
     })
@@ -129,10 +140,14 @@ if (isChecked === true) {
     this.audio = new Audio();
     this.audio.src = '../assets/mp3/prabhupadaChant.mp3';
     this.audio.load();
-    if (this.isAudioPassed) {
+    this.isPlaying = true;
+    if (this.isAudioPaused) {
       this.audio.play();
     } else {
       this.audio.pause();
+      this.dialogRef.close();
+      this.isPlayingsri = false;
+      this.isPlaying = false;
     }
     this.audio.addEventListener('ended', () => {
       this.playAudio(count + 1);
